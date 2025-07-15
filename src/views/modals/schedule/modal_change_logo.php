f1bab8ceb5d0abced66ffc50f21529ff219c75fa
Fernando Herrera <luisherreram324@gmail.com>
2025-07-15 11:53:29 -0600

    Add modal_change_logo.php for logo change functionality in modal

---
+++ src/views/modals/schedule/modal_change_logo.php
@@ -0,0 +1,11 @@
+    <!-- Modal Muestra-->
+    <div class="modal fade" id="modalChangeLogo" tabindex="-1" data-bs-keyboard="false" aria-labelledby="modalChangeLogoLabel" aria-hidden="false">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalChangeLogoLabel"></h5>
+                </div>
+                <div class="modal-body"></div>
+            </div>
+        </div>
+    </div>
