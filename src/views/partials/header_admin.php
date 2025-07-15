124373bfaf503b8cf739943c45cba59e1f561fff
Fernando Herrera <luisherreram324@gmail.com>
2025-07-15 11:52:43 -0600

    Add DataTables CSS for enhanced table styling in header_admin.php

--- src/views/partials/header_admin.php
+++ src/views/partials/header_admin.php
@@ -21,6 +21,7 @@ include dirname(__DIR__, 1) . '/partials/head_common.php';
     <link rel="stylesheet" href="<?= asset('public/css/modals.css') ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/2.3.2/css/dataTables.bootstrap5.css" rel="stylesheet">
 </head>
 
 <body data-page="<?= $pageId ?? '' ?>" class="protected">
