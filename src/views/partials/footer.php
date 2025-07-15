670ec49ad0763b4c8b10ea1570b45297a674b3e0
Fernando Herrera <luisherreram324@gmail.com>
2025-07-15 11:53:02 -0600

    Refactor footer.php to improve script organization and conditionally load DataTables and Fuse.js

--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -3,10 +3,21 @@
 <!-- <script>
     const BASE_URL = "<?= BASE_URL ?>";
 </script> -->
+
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://cdn.jsdelivr.net/npm/fuse.js@6.6.2"></script>
+
+<?php if ($request === 'admin/status'): ?>
+    <script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.3.2/js/dataTables.bootstrap5.js"></script>
+<?php endif; ?>
+
+<?php if ($request === 'admin/schedule'): ?>
+    <script src="https://cdn.jsdelivr.net/npm/fuse.js@6.6.2"></script>
+<?php endif; ?>
+
 <script src="<?= asset('public/js/auth_helper.js') ?>"></script>
 <script src="<?= asset('public/js/modals.js') ?>"></script>
 <script src="<?= asset('public/js/utils.js') ?>"></script>
