9008831c26d31527514d3e6e6ee4382e9b622c43
Fernando Herrera <luisherreram324@gmail.com>
2025-07-15 11:53:20 -0600

    Update modal_sample_client.php to enhance modal behavior and button functionality

--- src/views/modals/schedule/modal_sample_client.php
+++ src/views/modals/schedule/modal_sample_client.php
@@ -1,14 +1,13 @@
     <!-- Modal Muestra-->
-    <div class="modal fade" id="modalSample" tabindex="-1" aria-labelledby="modalSampleLabel" aria-hidden="true">
+    <div class="modal fade" id="modalSample" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="modalSampleLabel" aria-hidden="false">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="modalSampleLabel"></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body"></div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-success" data-bs-dismiss="modal"  id="btnConfirmSample">Confirmar Muestra</button>
                 </div>
             </div>
         </div>
