6c07fc9b95028eea4481f325a248e080116f5893
Fernando Herrera <luisherreram324@gmail.com>
2025-08-26 15:49:12 -0600

    fix: Correct URL paths and formatting in sidebar navigation

--- src/views/partials/sidebar.php
+++ src/views/partials/sidebar.php
@@ -1,20 +1,26 @@
    <div class="sidebar" id="sidebar">
-       <h5><a href="<?= BASE_URL?>/admin/dashboards">Home</a></h5>
+       <h5><a href="<?= BASE_URL ?>/admin/dashboards">Home</a></h5>
        <div class="section-title first">Aplicación</div>
-       <a href="<?= BASE_URL?>/admin/schedule">Programar</a>
-       <a href="<?= BASE_URL?>/admin/status">Estatus</a>
+       <a href="<?= BASE_URL ?>/admin/schedule">Programar</a>
+       <a href="<?= BASE_URL ?>/admin/status">Estatus</a>
 
        <div class="section-title">Resultados</div>
-       <a href="<?= BASE_URL?>/resultados/cliente">Cliente</a>
-       <a href="<?= BASE_URL?>/resultados/shelter">Shelter</a>
+       <a href="<?= BASE_URL ?>/results/clients">Cliente</a>
+       <a href="<?= BASE_URL ?>/results/shelter">Shelter</a>
+
+       <div class="section-title">Análisis</div>
+       <a href="<?= BASE_URL ?>/analysis/clients">Cliente</a>
+       <a href="<?= BASE_URL ?>/analysis/comparative_periods">Comparativo año</a>
+       <a href="<?= BASE_URL ?>/analysis/comparative_clients">Comparativo clientes</a>
+
 
        <div class="section-title">Seguimiento</div>
-       <a href="<?= BASE_URL?>/admin/tracker/client">Por cliente</a>
-       <a href="<?= BASE_URL?>/admin/specialistTracker">Por especialista</a>
+       <a href="<?= BASE_URL ?>/admin/tracker/client">Por cliente</a>
+       <a href="<?= BASE_URL ?>/admin/tracker/specialist">Por especialista</a>
 
        <div class="section-title">Administración</div>
-       <a href="<?= BASE_URL?>/admin/contacto">Información de contacto</a>
-       <a href="<?= BASE_URL?>/admin/cuestionarios">Cuestionarios clima</a>
-       <a href="<?= BASE_URL?>/admin/usuarios">Usuarios y Roles</a>
-       <a href="<?= BASE_URL?>/admin/reportes">Descargar reportes</a>
+       <a href="<?= BASE_URL ?>/admin/contact">Información de contacto</a>
+       <a href="<?= BASE_URL ?>/admin/questionnaires">Cuestionarios clima</a>
+       <a href="<?= BASE_URL ?>/admin/users">Usuarios y Roles</a>
+       <a href="<?= BASE_URL ?>/admin/reports">Descargar reportes</a>
    </div>
